ab2c8f1821df9c86b947dc7d70d6c6448ab0d93e
Anup Omkar <57665180+anupomkar@users.noreply.github.com>
2020-12-05 22:17:50 +0530

    Add files via upload

---
+++ config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGOURI:"mongodb+srv://anup_omkar:3J2d1G4G5Q8LKy30@cluster0.nubej.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    JWT_SECRET:"anupomkarmgmbestpesu"
+}
